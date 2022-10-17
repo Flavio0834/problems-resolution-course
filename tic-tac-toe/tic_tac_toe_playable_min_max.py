89a9cd84d88c9611e07488a75aa642ff2b643668
Sioka <adrien.soupiron@ecl21.ec-lyon.fr>
2022-10-17 15:50:53 +0200

    min_max algorithm implanted (IA may be flawed in the choice of the move)

--- tic-tac-toe/tic_tac_toe_playable_min_max.py
+++ tic-tac-toe/tic_tac_toe_playable_min_max.py
@@ -7,11 +7,18 @@ Created on Mon Oct 10 14:30:25 2022
 
 
 
-def interest(N,game,coord,color,case):
+def interest(N,game,color):
+    
+    if win(N, game, color):
+        return float("inf")
+    if win(N, game, not color):
+        return -float("inf")
+    
     ncolor = not color
     player, opponent=0,0
     for i in range(N):
         NL1,NL2,NC1,NC2=0,0,0,0
+        
         for j in range(N):
             if game[i][j]==color:
                 NL1+=1
@@ -21,10 +28,7 @@ def interest(N,game,coord,color,case):
                NC1+=1
             if game[j][i]==ncolor:
                 NC2+=1
-        if NL1==N or NC1==N:
-            return float("inf")
-        if NL2==N or NC2==N:
-            return -float("inf")
+                
         if NL1==0:
             opponent+=1
         if NL2==0:
@@ -33,69 +37,106 @@ def interest(N,game,coord,color,case):
             player+=1
         if NL1==0:
             opponent+=1 
-    return(player-opponent)
-
-
-        
             
+    ND11,ND12,ND21,ND22=0,0,0,0
+    for i in range(N):
+        if game[i][i]==color:
+            ND11+=1
+        if game[i][i]==ncolor:
+            ND12+=1
+        if game[N-1-i][i]==color:
+            ND21+=1
+        if game[N-1-i][i]==ncolor:
+            ND22+=1
+            
+    if ND11==0:
+        opponent+=1
+    if ND12==0:
+        player+=1
+    if ND22==0:
+        player+=1
+    if ND21==0:
+        opponent+=1 
     return(player-opponent)
-        
 
-def play(N, game, coord, human):
+def min_max(N,game,deep_org,deep,method,color):
+    if game!=[[float("inf") for i in range(N)] for j in range(N)]:
+        if deep==0 or win(N,game,not color):
+            score=interest(N,game,not color)
+            return game,score
+    min_score,max_score=float("inf"),-float("inf")
+    best_move=None
+    list_next_games=[]
+    #print(game[0].count(color))
+    Ncolor=sum([game[i].count(color) for i in range(N)])
+    if Ncolor<N:
+        list_next_games=next_game_less_3(N,game,color)
+    else:
+        list_next_games=next_game_more_3(N,game,color)
+    for game_bis in list_next_games:
+            game1,score=min_max(N, game_bis,deep_org, deep-1, not method, not color)
+            if method:
+                if score>max_score:
+                    best_score=score
+                    best_game= game1
+                    max_score = best_score
+            else:
+                if score<min_score:
+                    best_score=score
+                    best_game= game1
+                    min_score = best_score
+    if deep==deep_org:
+        return best_game,best_score
+    return game,best_score
+
+def next_game_less_3(N,game,color):
+    liste=[]
+    for i in range(N):
+        for j in range(N):  
+            if game[i][j]==float("inf"):
+                game_bis=[[element for element in line] for line in game]
+                game_bis[i][j]=color
+                liste.append(game_bis)
+    return liste
+
+def next_game_more_3(N,game,color):
+    liste=[]
+    for i in range(N):
+        for j in range(N):  
+            if game[i][j]==color:
+                liste_bis=next_game_less_3(N,game,color)
+                for game_bis in liste_bis:
+                    game_bis[i][j]=float("inf")
+                liste=liste+liste_bis
+    return liste
+    
+def maj_coord(N,game):
+    coord=[[],[],[]]
+    for i in range(N):
+        for j in range(N):
+            if game[i][j]==float("inf"):
+                coord[2].append((i,j))
+            elif game[i][j]:
+                coord[True].append((i,j))
+            elif not game[i][j]:
+                coord[False].append((i,j))
+    return coord
+
+def play(N, game, coord,deep, human):
     color = False
     end = False
     while not end:
         if color == human:
             place_human(N, game, coord, color)
         else:
-            place_best_first(N, game, coord, color)
+            game,score=min_max(N, game, deep, deep, True, color)
+            coord=maj_coord(N, game)
         end = win(N, game, color)
         if not end:
             color = not color
     return color
 
 
-def place_best_first(N, game, coord, color):
-    i, j = -1, -1
-    if len(coord[color]) == N:
-        mini = 0
-        minimums = []
-        for case in coord[color]:
-            g = evaluation(N, game, coord, color, case, True)
-            if g > mini:
-                mini = g
-        for case in coord[color]:
-            g = evaluation(N, game, coord, color, case, True)
-            if g == mini:
-                minimums.append(case)
-        i, j = minimums[0]
-    found = False
-    while not found:
-        maxi = 0
-        maximums = []
-        for case in coord[2]:
-            g = evaluation(N, game, coord, color, case, False)
-            if g > maxi:
-                maxi = g
-        for case in coord[2]:
-            g = evaluation(N, game, coord, color, case, False)
-            if g == maxi:
-                maximums.append(case)
-        x, y = -1, -1
-        for case in maximums:
-            if case[0] == case[1] or case[0] == N - 1 - case[1]:
-                x, y = case
-        if x < 0:
-            x, y = maximums[0]
-        if game[x][y] == float("inf"):
-            game[x][y] = color
-            coord[color].append((x, y))
-            coord[2].remove((x, y))
-            if i >= 0:
-                coord[color].remove((i, j))
-                coord[2].append((i, j))
-                game[i][j] = float("inf")
-            found = True
 
 
 def place_human(N, game, coord, color):
@@ -143,7 +184,7 @@ def win(N, game, color):
     return False
 
 
-def initialize(N):
+def initialize(N,deep):
     game = [[float("inf") for i in range(N)] for j in range(N)]
     coord = [[], [], []]
     for i in range(N):
@@ -154,12 +195,11 @@ def initialize(N):
         human = int(input("\nBlancs (0) ou Noirs (1) ? "))
     print(
         "\nLes Noirs ont gagné !\n"
-        if play(N, game, coord, human)
+        if play(N, game, coord, deep, human)
         else "\nLes Blancs ont gagné !\n"
     )
-    for k in game:
-        print(k)
+
 
 
 if __name__ == "__main__":
-    initialize(int(input("N = ? ")))
+    initialize(int(input("N = ? ")),int(input("Profondeur = ? ")))
